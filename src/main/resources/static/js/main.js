de44a53cd1c481b58c531c8227edb11e886ef57c
FFCucaracha <ffcucaracha@gmail.com>
2018-12-22 14:54:21 +0600

    try dialog, no

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,14 @@
 $(document).on('click', '#btn-bugaga', function() {
     $('.bazinga').show();
 });
+
+$(document).on("click", "[id^='open-dialog-']", function(){
+    var id_st = $(this).attr("id");
+    var id_ar = id_st.split('-');
+    var id = "#dialog-"+id_ar[id_ar.length-1];
+    //alert(id);
+    $(id).dialog({
+        autoOpen: false,
+    });
+    $(id).dialog("open");
+});
